77453a64e7a4fa918a604c83001efeefab190a1c
Muhammad-Nauman11 <muhammadnaumanna11@gmail.com>
2025-05-17 16:08:20 +0500

    lgin implimentation

--- ride-sharing-app/src/app/components/auth/login/login.component.ts
+++ ride-sharing-app/src/app/components/auth/login/login.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
   standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    // ✅ Safe to use `this.fb` here — it's initialized by Angular DI
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });}
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      
+      
+      // this.authService.login(this.loginForm.value).subscribe({
+      //   next: (res) => {
+      //     console.log('Login Success:', res);
+      //     // Store token or navigate
+      //   },
+      //   error: (err) => {
+      //     console.error('Login Failed:', err);
+      //   }
+      // });
+    }
+  }
 }
